e1fa0bbce39e0fa7f3f66f667e2aafc2c309e9d3
Beau Harrison <beau@fnal.gov>
2023-07-15 05:12:44 +0000

    Solution 1 complete. I learned some things!

--- Level-1/code.py
+++ Level-1/code.py
@@ -12,22 +12,40 @@
 '''
 
 from collections import namedtuple
+from decimal import Decimal
+
+MAX_ITEMS_IN_ORDER = 100
+MAX_ORDER_COST = 1e9
+MAX_ITEM_COST = 1e6
 
 Order = namedtuple('Order', 'id, items')
 Item = namedtuple('Item', 'type, description, amount, quantity')
 
+def is_reasonable_cost_gen(max_cost):
+    """Not empty"""
+    def worker(cost):
+        return cost > max_cost * -1 and cost < max_cost
+
+    return worker
+
+is_reasonable_order_cost = is_reasonable_cost_gen(MAX_ORDER_COST)
+is_reasonable_item_cost = is_reasonable_cost_gen(MAX_ITEM_COST)
+
 def validorder(order: Order):
+    """Not empty"""
     net = 0
-    
+
     for item in order.items:
         if item.type == 'payment':
-            net += item.amount
+            if is_reasonable_order_cost(item.amount):
+                net += Decimal(item.amount)
         elif item.type == 'product':
-            net -= item.amount * item.quantity
+            if is_reasonable_item_cost(item.amount) and item.quantity < MAX_ITEMS_IN_ORDER:
+                net -= Decimal(item.amount) * Decimal(item.quantity)
         else:
-            return("Invalid item type: %s" % item.type)
-    
-    if net != 0:
-        return("Order ID: %s - Payment imbalance: $%0.2f" % (order.id, net))
-    else:
-        return("Order ID: %s - Full payment received!" % order.id)
+            return f"Invalid item type: {item.type}"
+
+    if net < 0:
+        return f"Order ID: {order.id} - Payment imbalance: ${net:0.2f}"
+
+    return f"Order ID: {order.id} - Full payment received!"
